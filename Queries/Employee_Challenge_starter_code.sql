45d5aafaca5d71d71bcdc228eb4adf2283af7e10
Miracle Onyeoziri <miracleonyeoziri@Miracles-MBP.home>
2023-10-30 17:34:34 +0100

    Imported CSV files from PostreSQL

--- Queries/Employee_Challenge_starter_code.sql
+++ Queries/Employee_Challenge_starter_code.sql
@@ -8,3 +8,10 @@ INTO nameyourtable
 FROM _______
 WHERE _______
 ORDER BY _____, _____ DESC;
+
+
+Deliverable 1:
+- create a Retirement Titles table that holds all the titles of employees who were born between January 1, 1952 and December 31, 1955
+- use the DISTINCT ON statement to create a table that contains the most recent title of each employee
+- use the COUNT() function to create a table that has the number of retirement-age employees by most recent job title
+- exclude employees who have already left the company
