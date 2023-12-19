be6ee18cfffe7e82ea5c79d0fbf4241c7d9cf73c
Miracle Onyeoziri <miracleonyeoziri@Miracles-MBP.home>
2023-12-19 13:04:05 +0100

    Adding new tables, images, and updated query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -9,7 +9,7 @@ SELECT e.emp_no,
 INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as ti
-ON e.emp_no = ti.emp_no
+ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
@@ -30,7 +30,8 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT (ut.title) DESC;
 
--- reviewing table
+-- reviewing tables
+SELECT * FROM unique_titles;
 SELECT * FROM retiring_titles;
 
 -- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
@@ -51,3 +52,154 @@ ON e.emp_no = ti.emp_no
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
+
+-- reviewing table
+SELECT * FROM mentorship_eligibility;
+
+
+
+-- DATA EXPLORATION: SUPPORTING QUERIES for Deliverable 1 -- 
+-- Creating table of total company employees (TCE)
+SELECT DISTINCT ON (ti.emp_no) ti.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO dft_tot_emp
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE ti.to_date = ('9999-01-01')
+ORDER BY ti.emp_no, ti.to_date DESC;
+
+-- Creating table of TCE by title
+SELECT COUNT (tce.emp_no), tce.title
+INTO active_emp
+FROM dft_tot_emp as tce
+GROUP BY tce.title
+ORDER BY COUNT (tce.title) DESC;
+
+-- reviewing tables
+SELECT * FROM dft_tot_emp;
+SELECT * FROM active_emp;
+
+-- counting total company employees (TCE) by title (n= 240,124)
+SELECT COUNT (tce.title) as total
+FROM dft_tot_emp as tce;
+
+-- counting total retiring employees (n = 72,458)
+SELECT COUNT (emp_no) as total
+FROM unique_titles;
+
+-- TABLE: percentage of retiring employees per title (IMAGE A)
+SELECT count, rt.title,
+ROUND(CAST(count AS DECIMAL(7, 2)) * 100/72458, 3) as "percent of total (%)"
+INTO rt_title_percent
+FROM retiring_titles as rt;
+
+-- counting total retiring engineers (n = 36,291)
+SELECT SUM (count)
+FROM retiring_titles as rt
+WHERE rt.title LIKE '%ngineer%';
+
+-- creating table of retiring employee by department using inner and left join (IMAGE B)
+SELECT COUNT (ut.emp_no), 
+	d.dept_name
+INTO retiring_dept
+FROM unique_titles as ut
+LEFT JOIN dept_emp as de
+ON ut.emp_no = de.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+GROUP BY de.dept_no, d.dept_name
+ORDER BY de.dept_no;
+
+-- reviewing table
+SELECT * FROM unique_titles;
+SELECT * FROM retiring_titles;
+SELECT * FROM rt_title_percent;
+SELECT * FROM retiring_dept;
+
+-- percentage (included) of retiring per department
+SELECT count, rd.dept_name,
+	 ROUND (CAST (count AS DECIMAL(7, 2)) * 100/72458, 3) as "percent of total (%)"
+FROM retiring_dept as rd
+INNER JOIN departments as d
+ON rd.dept_name = d.dept_name
+ORDER BY count DESC;
+
+
+
+-- SUPPORTING QUERIES for Deliverable 2 --
+-- counting total employees in mentorship eligible (ME) program (n= 1,549)
+SELECT COUNT(*) FROM mentorship_eligibility; 
+
+-- creating table for ME by title
+SELECT COUNT (me.emp_no), me.title
+INTO mentor_title
+FROM mentorship_eligibility as me
+GROUP BY me.title
+ORDER BY COUNT (me.title) DESC;
+
+-- percentage of ME per title (IMAGE C)
+SELECT count, mbt.title,
+ROUND(CAST(count AS DECIMAL(7, 2)) * 100/1549, 3) as "percent of total (%)"
+INTO mentor_title_percent
+FROM mentor_title as mbt;
+
+-- use dictinct with orderby to remove duplicate rows in dept_emp
+SELECT DISTINCT ON (de.emp_no) de.emp_no,
+de.dept_no
+INTO current_dept
+FROM dept_emp as de
+WHERE de.to_date = ('9999-01-01')
+ORDER BY de.emp_no;
+
+-- reviewing tables 
+SELECT * FROM mentorship_eligibility;
+SELECT * FROM dept_emp; -- (x10 (d004, d006), x18 (etc.), x29 (etc.) had duplicates)
+SELECT * FROM current_dept;
+SELECT * FROM mentor_title;
+SELECT * FROM mentor_title_percent;
+
+-- creating table of mentors by department using left and inner join (IMAGE D)
+SELECT COUNT (me.emp_no), 
+	cd.dept_no,
+	d.dept_name
+INTO mentor_dept
+FROM mentorship_eligibility as me
+LEFT JOIN current_dept as cd
+ON me.emp_no = cd.emp_no
+INNER JOIN departments as d
+ON cd.dept_no = d.dept_no
+GROUP BY cd.dept_no, d.dept_name
+ORDER BY cd.dept_no;
+
+-- reviewing tables
+SELECT * FROM mentor_title;
+SELECT * FROM mentor_dept;
+SELECT * FROM mentor_dept_percent;
+SELECT * FROM employees_dept_and_title;
+
+-- percentage of mentors per department
+SELECT count, d.dept_name,
+	 ROUND (CAST (count AS DECIMAL(7, 2)) * 100/1549, 3) as "percent of total (%)"
+INTO mentor_dept_percent
+FROM mentor_dept as md
+INNER JOIN departments as d
+ON md.dept_no = d.dept_no
+ORDER BY count DESC;
+
+-- creating table of both retiring and mentoring employees by department and title
+SELECT COUNT(ut.emp_no) AS title_count,
+       d.dept_name,
+       ut.title
+INTO employees_dept_and_title
+FROM unique_titles AS ut
+LEFT JOIN dept_emp AS de 
+ON ut.emp_no = de.emp_no
+INNER JOIN departments AS d 
+ON de.dept_no = d.dept_no
+GROUP BY d.dept_name, ut.title
+ORDER BY d.dept_name, title_count DESC;
--- README.md
+++ README.md
@@ -2,15 +2,42 @@
 
 ## Overiew
 
-Determine the number of retiring employees per title, and identify employees who are eligible to participate in a mentorship program.
+The purpose of this analysis is to determine the number of retiring Pewlett-Hackard employees per title and identify retiring-employees who are eligible to participate in a mentorship program. The mentorship program is designed to pair experienced and successful employees with new hires. This allows retirement-eligible employees to step back into a part-time role instead of retiring completely.
 
 ## Review
 
-Provide a bulleted list with four major points from the two analysis deliverables. Use images as support where needed.
+Retirement-eligible employees are employees born between 01/1952 and 12/1995.
+- Pewlett-Hackared has 240,124 employees of which 30% (72,458) are retirement-eligible. 
+- 50% (36,291) of retirement-eligible employees work as Engineers.
+- About 70% of retirement-eligible employees hold a senior-level position.
+- 50% of senior-level employees hold either a Senior Engineer (35.8%) or a Senior Staff (34.4%) position.
+
+Image A: Retiring Employees by Title
+![Alt Retiring Employees by Title](image link)
 
 ## Summary
 
-Provide high-level responses to the following questions, then provide two additional queries or tables that may provide more insight into the upcoming "silver tsunami."
+As the "silver tsunami" begins to make an impact, two roles will need to be filled: Senior Engineer and Senior Staff. These two positions hold the most employees who are nearing retirment.
 
-- How many roles will need to be filled as the "silver tsunami" begins to make an impact?
-- Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
+Table 1: Retiring Employees by Title
+![Alt Retiring Employees by Title](image link)
+
+Table 2: Mentors by Title 
+![Alt Employees in Mentorship Program by Title](image link)
+
+In the current mentorship program, there are 1,549 qualified, retirement-ready employees. 36.7% hold a Senior Staff position while only 10.9% of mentors hold a Senior Engineer position. There is not enough qualified Senior Engineers, however, there are enough employees in each department.
+
+Below are graphs of retiring employees and mentors by title and department.
+
+
+Graph A
+![Alt Retiring Employees by Title: Bar Graph](image link)
+
+Graph A.I
+![Alt Mentors by Title: Bar Graph](image link)
+
+Graph B
+![Alt Retiring Employees by Department: Bar Graph](image link)
+
+Graph B.I
+![Alt Mentors by Department: Bar Graph](image link)
